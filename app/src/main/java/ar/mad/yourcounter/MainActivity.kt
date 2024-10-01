d8096d11c1a24876637f84011f183754d1f0a70e
Maximiliano Drago <maximilianoadrago@gmail.com>
2024-09-30 22:42:52 -0300

    First commit - base

---
+++ app/src/main/java/ar/mad/yourcounter/MainActivity.kt
@@ -0,0 +1,36 @@
+package ar.mad.yourcounter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import ar.mad.yourcounter.ui.theme.YourCounterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            YourCounterTheme {
+                val navController = rememberNavController()
+                AppNavHost(navController = navController)
+            }
+        }
+    }
+}
+
+@Composable
+fun AppNavHost(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = "splash") {
+        composable("splash") { SplashScreen(navController) }
+        composable("login") { LoginScreen(navController) }
+        composable("home") { HomeScreen(navController) }
+        composable("counter/{counterId}") { backStackEntry ->
+            val counterId = backStackEntry.arguments?.getString("counterId")
+            counterId?.let { CounterScreen(navController, it) }
+        }
+    }
+}
